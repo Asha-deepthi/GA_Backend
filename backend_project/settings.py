3bf04860739f989f0f06e7dc3c881c1d2cd680f4
Asha <202040257asha@gmail.com>
2025-05-23 11:30:43 +0530

    shifted from postgresql to dbsqlite

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -109,14 +109,11 @@ WSGI_APPLICATION = 'backend_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ga_backend_db',
-        'USER': 'ga_backend_user',
-        'PASSWORD': 'securepass',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 
