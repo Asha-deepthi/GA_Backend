2e6e6d3f80f314f157c7951aaefc30f4e2f832c7
morthakalyani <morthakalyani05@gmail.com>
2025-05-30 14:02:59 +0530

    added phone number validation

--- users/urls.py
+++ users/urls.py
@@ -9,10 +9,11 @@
 #backend/users/urls.py
 import uuid
 from django.urls import path
-from .views import SendVerificationEmailView, VerifyEmailView, CustomTokenObtainPairView
+from .views import SendVerificationEmailView, VerifyEmailView, EmailOrPhoneLoginView
 
 urlpatterns = [
     path('send-verification-email/', SendVerificationEmailView.as_view(), name='send_verification_email'),
-    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    #path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('verify-email/<uuid:uuid>/', VerifyEmailView.as_view(), name='verify_email'),
+     path('login/', EmailOrPhoneLoginView.as_view(), name='email-or-phone-login'),
 ]
